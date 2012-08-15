fb486e943800a41e3eebbd3ad8673bbf01bbf951
brendanhay <brendan.g.hay@gmail.com>
2012-08-15 14:58:33 +0200

    Adding proxy supervisor

--- src/vanguard_sup.erl
+++ src/vanguard_sup.erl
@@ -37,4 +37,7 @@ start_link() -> supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 init([]) ->
     %% Ensure the listeners are linked against the supervisor process
     ok = vanguard_listener:start_link(),
-    {ok, {{one_for_all, 3, 20}, []}}.
+    FsmSup = {vanguard_proxy_sup,
+              {vanguard_proxy_sup, start_link, []},
+              permanent, 5000, supervisor, [vanguard_proxy_sup]},
+    {ok, {{one_for_one, 5, 10}, [FsmSup]}}.
