79a1f27c9a0bdf1eddfa0d45f2a8c5fd101cf72e
brendanhay <brendan.g.hay@gmail.com>
2012-08-15 15:57:18 +0200

    Adding type specs

--- src/vanguard_handler.erl
+++ src/vanguard_handler.erl
@@ -22,10 +22,15 @@
 %% Callbacks
 %%
 
+-spec init({tcp, http}, #http_req{}, [backend()])
+    -> {loop, #http_req{}, reference(), non_neg_integer(), hibernate}.
+%% @hidden
 init({tcp, http}, Req, Backends) ->
     {ok, ProxyId} = vanguard_proxy:forward(Backends, Req),
     {loop, Req, ProxyId, ?TIMEOUT * 2, hibernate}.
 
+-spec info(_, #http_req{}, reference()) -> {ok | loop, #http_req{}, reference()}.
+%% @hidden
 info({ok, ProxyId, Status, Body}, Req, ProxyId) ->
     lager:info("REPLY ~p ~p", [ProxyId, self()]),
     {ok, NewReq} = cowboy_http_req:reply(Status, [], Body, Req),
@@ -34,6 +39,8 @@ info(Msg, Req, ProxyId) ->
     lager:error("UNHANDLED ~p ~p ~p", [Msg, ProxyId, self()]),
     {loop, Req, ProxyId}.
 
+-spec terminate(#http_req{}, reference()) -> ok.
+%% @hidden
 terminate(_Req, _ProxyId) ->
     lager:info("TERMINATE ~p", [self()]),
     ok.
